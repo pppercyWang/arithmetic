3305bebbf042ef502f67e6da9342874ac4ed60d4
pppercyWang <pppercywang@163.com>
2024-03-14 12:12:48 +0800

    fix: bind

---
+++ 数组和字符串/bind.js
@@ -0,0 +1,16 @@
+Function.prototype.myBind = function(obj, ...arg){
+    let fn = this;
+    return function(){
+        fn.apply(obj, [...arg, ...arguments])
+    }
+}
+
+
+
+
+function test(a, b, c){
+    console.log(this.name, a, b, c, '--------')
+}
+
+const a = test.myBind({name: 'ddd'} , 1,2)
+a(5)
